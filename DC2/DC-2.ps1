d2cfa06c06331d2b614caec29ce9d2b7021eee9d
Eli Zolberg <zolbergeli@gmail.com>
2022-08-14 14:50:10 +0300

    Update DC-2.ps1

--- DC2/DC-2.ps1
+++ DC2/DC-2.ps1
@@ -33,7 +33,7 @@ $ipIF = (Get-NetAdapter -Name "Ethernet").ifIndex
 netsh interface ip set address $ipIF static 10.0.0.5 255.255.255.0 10.0.0.1
 Set-DnsClientServerAddress -InterfaceIndex $ipIF -ServerAddresses 10.0.0.4
 
-
+#Adding secondary domain controller 
 Install-WindowsFeature AD-Domain-Services –IncludeManagementTools
 Get-Command -Module ADDSDeployment
 Install-ADDSDomainController -DomainName "azuretesing.com" -InstallDns -Credential (get-credential AZURETESING\azureteadmin) -DatabasePath "D:\ADDS\DB" -LogPath "D:\ADDS\Log" -SysvolPath "D:\ADDS\SYSVOL"
